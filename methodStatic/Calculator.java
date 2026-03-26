af3acf9af25231b38a3a86c45e292178f0583377
NishantShastri <nishantshastri2004@gmail.com>
2026-03-26 17:31:18 +0530

    Update Calculator.java

--- methodStatic/Calculator.java
+++ methodStatic/Calculator.java
@@ -2,6 +2,11 @@ package methodStatic;
 
 public class Calculator {
 
+	/* In static method the input is already put in this backend and the result will be out in frontend
+	 * Here the customer/user cant interact with the machine or type any input as they can only see the results
+	 * This can only be changed by the developer
+	 */
+
 	public int myAddition() {
 
 		int a = 6;
